3c80799064d5e63192eb1eb436b365360b2cb330
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-06-03 14:54:36 +0530

    Create basic alarm functionality

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// referencing all the elements
 const currentTime = document.getElementById('current_time'); // current time element
+const alarmLabel = document.getElementById('alarm_label'); // alarm label input 
 const hoursMenu = document.getElementById('hours_menu'); // hours menu dropdown
 const minutesMenu = document.getElementById('mins_menu'); // minutes menu dropdown
 const secondsMenu = document.getElementById('secs_menu'); // seconds menu dropdown
+const meridianMenu = document.getElementById('meridian_menu') // meridian menu dropdown
+const setAlarm = document.getElementById('set_alarm'); // set alarm button
 
 // to add zero infront of time values less than 10
 function extraZero(value){
@@ -58,3 +62,43 @@ function addSeconds() {
 }
 
 addSeconds();
+
+// setting an alarm
+setAlarm.addEventListener('click',function(){
+    let selectedHour = hoursMenu.value;
+    let selectedMinutes = minutesMenu.value;
+    let selectedSeconds = secondsMenu.value;
+    let selectedMeridian = meridianMenu.value;
+    let givenLabel = alarmLabel.value;
+
+    if(givenLabel === ""){
+        givenLabel = "Alarm"
+    }
+    
+    // calculating the alarm time
+    let alarmTime = extraZero(selectedHour) + ":" + extraZero(selectedMinutes) + ":" + extraZero(selectedSeconds) + "  " + selectedMeridian;
+    
+    console.log(`Setted an alarm for ${givenLabel} at ${alarmTime}`);
+
+    // matching current time and the alarm time
+    setInterval(function(){
+        let date = new Date(); // new date instance
+        let hours = date.getHours(); // hour value
+        let minutes = date.getMinutes(); // minutes value
+        let seconds = date.getSeconds(); //seconds value
+        let meridian = (date.getHours()) < 12 ? 'AM' : 'PM'; // meridian
+    
+        // handling hours checks
+        if(hours <= 12){
+            hours = hours 
+        }else{
+            hours = hours - 12;
+        }
+    
+        let current_Time = currentTime.textContent = extraZero(hours) + ":" + extraZero(minutes) + ":" + extraZero(seconds) + "  " + meridian;
+
+        if(alarmTime == current_Time){
+            window.alert(givenLabel);
+        }
+    },1000);
+});
