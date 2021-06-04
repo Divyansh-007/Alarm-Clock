3171bb145f8fd5b127575317cbf7f68435f3502a
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-06-04 09:14:35 +0530

    Add all functionalities

--- script.js
+++ script.js
@@ -63,7 +63,11 @@ function addSeconds() {
 
 addSeconds();
 
-// setting an alarm
+showAllAlarms();
+
+fireAlarms();
+
+// setting an alarm and adding it to the list of upcoming alarms
 setAlarm.addEventListener('click',function(){
     let selectedHour = hoursMenu.value;
     let selectedMinutes = minutesMenu.value;
@@ -77,28 +81,106 @@ setAlarm.addEventListener('click',function(){
     
     // calculating the alarm time
     let alarmTime = extraZero(selectedHour) + ":" + extraZero(selectedMinutes) + ":" + extraZero(selectedSeconds) + "  " + selectedMeridian;
-    
-    console.log(`Setted an alarm for ${givenLabel} at ${alarmTime}`);
 
-    // matching current time and the alarm time
-    setInterval(function(){
-        let date = new Date(); // new date instance
-        let hours = date.getHours(); // hour value
-        let minutes = date.getMinutes(); // minutes value
-        let seconds = date.getSeconds(); //seconds value
-        let meridian = (date.getHours()) < 12 ? 'AM' : 'PM'; // meridian
+    let event = {
+        label : givenLabel,
+        time: alarmTime
+    }
     
-        // handling hours checks
-        if(hours <= 12){
-            hours = hours 
-        }else{
-            hours = hours - 12;
-        }
-    
-        let current_Time = currentTime.textContent = extraZero(hours) + ":" + extraZero(minutes) + ":" + extraZero(seconds) + "  " + meridian;
+    let upcomingAlarms = localStorage.getItem('upcomingAlarms');
 
-        if(alarmTime == current_Time){
-            window.alert(givenLabel);
-        }
-    },1000);
+    if(upcomingAlarms == null){
+        alarmsList = [];
+    }else{
+        alarmsList = JSON.parse(upcomingAlarms);
+    }
+
+    alarmsList.push(event);
+    localStorage.setItem('upcomingAlarms',JSON.stringify(alarmsList));
+
+    showAllAlarms();
+    fireAlarms();
 });
+
+// deleting an alarm from upcoming alarms list
+function deleteAlarm(index){
+    let upcomingAlarms = localStorage.getItem('upcomingAlarms');
+    let alarmsList = JSON.parse(upcomingAlarms);
+
+    alarmsList.splice(index,1);
+
+    localStorage.setItem('upcomingAlarms',JSON.stringify(alarmsList));
+
+    showAllAlarms();
+    fireAlarms();
+}
+
+// show all upcoming alarms list
+function showAllAlarms(){
+    let upcomingAlarms = localStorage.getItem('upcomingAlarms');
+
+    if(upcomingAlarms == null){
+        alarmsList = [];
+    }else{
+        alarmsList = JSON.parse(upcomingAlarms);
+    }
+
+    let html = '';
+    let upcomingAlarmsList = document.getElementById('alarms_list');
+
+    upcomingAlarmsList.innerHTML = html;
+
+    alarmsList.forEach((event,index) =>{
+        html += `<tr>
+                    <td class="alarm_info">
+                        <p style="margin: 5px;">${event.label}</p>
+                        <p><i class="far fa-clock"></i> ${event.time}</p>
+                    </td>
+                    <td class="del_btn" onclick="deleteAlarm(${index})"><i class="fas fa-trash-alt fa-lg"></i></td>
+                </tr>`
+    });
+
+    upcomingAlarmsList.innerHTML = html;
+}
+
+// firing the alarms from upcoming ones in the order
+function fireAlarms(){
+    let upcomingAlarms = localStorage.getItem('upcomingAlarms');
+
+    if(upcomingAlarms == null){
+        alarmsList = [];
+    }else{
+        alarmsList = JSON.parse(upcomingAlarms);
+    }
+
+    if(alarmsList.length != 0){
+        let nextAlarm = alarmsList[0];
+        console.log(nextAlarm);
+
+        // matching current time and the alarm time
+        setInterval(function(){
+            let date = new Date(); // new date instance
+            let hours = date.getHours(); // hour value
+            let minutes = date.getMinutes(); // minutes value
+            let seconds = date.getSeconds(); //seconds value
+            let meridian = (date.getHours()) < 12 ? 'AM' : 'PM'; // meridian
+        
+            // handling hours checks
+            if(hours <= 12){
+                hours = hours 
+            }else{
+                hours = hours - 12;
+            }
+        
+            let current_Time = currentTime.textContent = extraZero(hours) + ":" + extraZero(minutes) + ":" + extraZero(seconds) + "  " + meridian;
+
+            if(nextAlarm.time == current_Time){
+                window.alert(nextAlarm.label);
+                alarmsList.splice(0,1);
+                localStorage.setItem('upcomingAlarms',JSON.stringify(alarmsList));
+                showAllAlarms();
+                fireAlarms();
+            }
+        },1000);
+    }
+}
